e9917fe05cab23878070bd2834e5e104e95eb4e4
SergijPSO <pankivsergij97@gmail.com>
2018-09-18 11:14:13 +0300

    lviv coordinates was added to the map

--- js/index.js
+++ js/index.js
@@ -15,12 +15,18 @@
 //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
 //     .openPopup();
 
-var map = L.map('map').setView([49.83826, -24.02324], 13);
+var map = L.map('map').setView([49.835715, 24.060306
+], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([49.83826, -24.02324]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+L.marker([49.835715, 24.060306
+]).addTo(map)// маркер котика-7
+.bindPopup('HappyWalker Head Quaqrter is here')
+.openPopup();
+
+// L.marker([49.83826, 24.02324]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
